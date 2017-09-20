5fb33242c9cbadff88a2d94ebbee86c185a2458c
nicolaslee098 <nicolaslee888@yahoo.com>
2017-09-20 13:41:27 +0700

    hw

---
+++ Hw.py
@@ -0,0 +1,12 @@
+var = 'abcba'
+var2 = 'hello'
+var3 = 'abcba'[::-1]
+var4 = 'hello' [::-1]
+if (var) ==(var3):
+    print ('it is a palindrome')
+else:
+    print ('it is not a palindrome')
+if (var2) ==(var4):
+    print ('it is a palindrome')
+else:
+    print ('it is not a palindrome')
---
+++ hw- fibo.py
@@ -0,0 +1,15 @@
+list = []
+for a in range (0, 100):
+    if (a%2)==0:
+        list.append(a)
+        (str(a))
+        #str = string
+print (list)
+
+list= []
+count = 0
+while (count<999):
+    print (str(count))
+    count = count +3
+    list.append(count)
+print (list, end=', ')
